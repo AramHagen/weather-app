f59973e2374b349a05852a9e6db48b6ffa6c01f7
Aram Hagen <aram.hagen@gmail.com>
2024-11-28 11:36:18 -0800

    feat: login page and validations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,52 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html", "./src/**/*.{scss,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./*.html", "./styles/*.scss", "./scripts/*.js", "./src/**/*.{scss,js}"],
+    theme: {
+        screens: {
+            sm: '480px',
+            md: '768px',
+            lg: '976px',
+            xl: '1280px',
+            '2xl': '1536px',
+        },
+        extend: {
+            colors: {
+                brightRed: 'hsl(12, 88%, 59%)',
+                brightRedLight: 'hsl(12, 88%, 69%)',
+                brightRedSupLight: 'hsl(12, 88%, 95%)',
+                darkBlue: 'hsl(228, 39%, 23%)',
+                darkGrayishBlue: 'hsl(227, 12%, 61%)',
+                veryDarkBlue: 'hsl(233, 12%, 13%)',
+                veryPaleRed: 'hsl(13, 100%, 96%)',
+                veryLightGray: 'hsl(0, 0%, 98%)',
+                violentViolet: '#240B48',
+                blackcurrant: '#190733',
+                paua: '#1F1D47'
+            },
+            fontFamily: {
+                vietnam: ['"Be Vietnam Pro"', 'sans-serif'],
+            },
+        },
+        fontSize: {
+            sm: '0.8rem',
+            base: '1rem',
+            xl: '1.25rem',
+            '2xl': '1.563rem',
+            '3xl': '1.953rem',
+            '4xl': '2.441rem',
+            '5xl': '3.052rem',
+            '6xl': '4.75rem',
+        },
+        lineHeight: {
+            '15': '3.75rem', // 60px (custom line-height)
+            '12': '3rem',    // 48px (optional)
+            '9': '2.25rem',  // 36px
+            '6': '1.5rem',   // 24px
+        },
+        fontFamily: {
+            'inter': ['Inter', 'sans-serif']
+        }
+    },
+    plugins: [],
 }
 
